87783e8902a724cd7a6839cc4f73ac098b092e43
piotrmaliga-git <piotr.maliga.work@gmail.com>
2026-04-03 18:54:06 +0200

    fix(routes): update import paths to include leading '@' for consistency

--- phishing-email-analyzer/frontend/src/app/routes/app.routes.ts
+++ phishing-email-analyzer/frontend/src/app/routes/app.routes.ts
@@ -3,19 +3,19 @@ import { type Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('pages/home/home.component').then((m) => m.HomePageComponent),
+    loadComponent: () => import('@pages/home/home.component').then((c) => c.HomePageComponent),
   },
   {
     path: 'pl',
-    loadComponent: () => import('pages/home/home.component').then((m) => m.HomePageComponent),
+    loadComponent: () => import('@pages/home/home.component').then((c) => c.HomePageComponent),
   },
   {
     path: 'en',
-    loadComponent: () => import('pages/home/home.component').then((m) => m.HomePageComponent),
+    loadComponent: () => import('@pages/home/home.component').then((c) => c.HomePageComponent),
   },
   {
     path: '**',
     loadComponent: () =>
-      import('pages/not-found/not-found.component').then((m) => m.NotFoundPageComponent),
+      import('@pages/not-found/not-found.component').then((c) => c.NotFoundPageComponent),
   },
 ];
