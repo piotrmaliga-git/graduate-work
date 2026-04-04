08a10a72a54704a529c38576b41ae798f6d074e9
piotrmaliga-git <piotr.maliga.work@gmail.com>
2026-04-04 17:04:20 +0200

    fix(routes): remove 'en' route to streamline language options

--- phishing-email-analyzer/frontend/src/app/routes/app.routes.ts
+++ phishing-email-analyzer/frontend/src/app/routes/app.routes.ts
@@ -10,10 +10,6 @@ export const routes: Routes = [
     loadComponent: () => import('@pages/home/home.component').then((c) => c.HomePageComponent),
   },
   {
-    path: 'en',
-    loadComponent: () => import('@pages/home/home.component').then((c) => c.HomePageComponent),
-  },
-  {
     path: '**',
     loadComponent: () =>
       import('@pages/not-found/not-found.component').then((c) => c.NotFoundPageComponent),
