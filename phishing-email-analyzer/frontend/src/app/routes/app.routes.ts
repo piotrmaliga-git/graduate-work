28d61919c912f894d190ddc9e89d3d976e745941
piotrmaliga-git <piotr.maliga.work@gmail.com>
2026-04-06 22:39:15 +0200

    fix(routes): remove redundant 'pl' route for home component

--- phishing-email-analyzer/frontend/src/app/routes/app.routes.ts
+++ phishing-email-analyzer/frontend/src/app/routes/app.routes.ts
@@ -6,10 +6,6 @@ export const routes: Routes = [
     loadComponent: () => import('@pages/home/home.component').then((c) => c.HomePageComponent),
   },
   {
-    path: 'pl',
-    loadComponent: () => import('@pages/home/home.component').then((c) => c.HomePageComponent),
-  },
-  {
     path: '**',
     loadComponent: () =>
       import('@pages/not-found/not-found.component').then((c) => c.NotFoundPageComponent),
